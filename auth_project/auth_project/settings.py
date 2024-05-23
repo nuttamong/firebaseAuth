af97ce3407f6be0f365e2d9d80aea0f8c20cb77b
nuttamong <nuttamong@kkumail.com>
2024-05-23 16:05:12 +0700

    complete

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'auth_project.wsgi.app'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "",
+        'USER': "",
+        'PASSWORD': "",
+        'HOST': "",
+        'PORT': "",
     }
 }
 
